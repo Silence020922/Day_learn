3ad35c66ae07e35e6ada1168b99b9b2561a96f4a
Silence020922 <1367896482@qq.com>
2024-01-31 18:40:05 +0800

    + early stopping

--- low_level_gcn/main.py
+++ low_level_gcn/main.py
@@ -20,11 +20,12 @@ flags.DEFINE_float("weight_decay", 5e-4, "Weight for L2 loss on embedding matrix
 flags.DEFINE_string("kernel_init", "glorot", "Initial weights method.")
 flags.DEFINE_integer("train_set_size", 140, "Number of train set.")
 flags.DEFINE_integer("repeat_num", 1, "Number of repetitions.")
+flags.DEFINE_integer("early_stopping", 30, "Early stopping.")
 test_acc_list = []
 test_loss_list = []
 t_test = time.time()
 for _ in range(FLAGS.repeat_num): # 做重复实验
-    print('------{}%------'.format(_))
+    print('------{}%------'.format((_)*100/FLAGS.repeat_num))
     adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_dataset(
         FLAGS.dataset,FLAGS.train_set_size
     )
@@ -51,18 +52,25 @@ for _ in range(FLAGS.repeat_num): # 做重复实验
 
     optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
 
-
+    epoch_loss = list()
     for epoch in range(FLAGS.epochs):
         with tf.GradientTape() as tape:
             loss, acc = model((features, train_label, train_mask,support))
         grads = tape.gradient(loss, model.trainable_variables)
         optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        val_loss, val_acc = model((features, val_label, val_mask, support),training = False)
+        epoch_loss.append(val_loss)
+
+        # Early stop
+        if epoch > FLAGS.early_stopping and epoch_loss[-1] > np.mean(epoch_loss[-(FLAGS.early_stopping+1):-1]): 
+            print("Early stopping..., epoch: {}".format(epoch))
+            break
     test_loss,test_acc = model((features,test_label,test_mask,support),training = False)
     test_loss_list.append(test_loss)
     test_acc_list.append(test_acc)
+
 print("Test set results({} mean):".format(FLAGS.repeat_num), "cost=", "{:.5f}".format(np.array(test_loss_list).mean()),
 "accuracy=", "{:.5f}".format(np.array(test_acc).mean()), "time=", "{:.5f}".format(time.time() - t_test))
-        # val_loss, val_acc = model((features, val_label, val_mask, support),training = False)
 
     # print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(float(loss)),
     #     "train_acc=", "{:.5f}".format(float(acc)), "val_loss=", "{:.5f}".format(float(val_loss)),
