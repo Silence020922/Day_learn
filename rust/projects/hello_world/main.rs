30117fb8c8a450a9eb22aae3f9b33b3be6ab3625
Silence020922 <1367896482@qq.com>
2024-05-09 14:53:11 +0800

    rust

--- rust/projects/hello_world/main.rs
+++ rust/projects/hello_world/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello,world!");
+    let s = String::from("Hello");
+    let s = change(s);
+    println!("{s}");
+}
+
+fn change(mut some_string: String)->String{
+    some_string.push_str(", world.");
+    some_string
 }
